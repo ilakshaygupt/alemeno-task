a8a57d222a188c211aed2f528efaeb844670e69a
lakshay <gupta22154006@akgec.ac.in>
2024-06-13 17:11:47 +0530

    fix docker dependencies and changed urls

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ COPY requirements.txt .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-RUN apt-get update 
+RUN apt-get update && apt-get install -y libgl1-mesa-glx
 
 COPY . .
 
--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ImageUploadView, index
 
 urlpatterns = [
-    path("index/", index, name="index"),
+    path("", index, name="index"),
     path("urine-analyze/", ImageUploadView.as_view(), name="urine-analyze"),
 ]
